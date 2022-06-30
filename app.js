928d4570de74d9361f0057e02e2b98cc135a7272
Sue Young <m.sue.young@gmail.com>
2022-06-30 16:15:45 -0600

    typo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let userAge = null;
  
 while (userAge == null || userAge == "") {  
-   userAge = prompt("Enter your age to find out how much younger you could be with.\nThis pop-up will annoy you until you do!");
+   userAge = prompt("Enter your age to find out how much younger you could be with a Yoga practice.\nThis pop-up will annoy you until you do!");
 }
       
  function reverseAge( age ) {
